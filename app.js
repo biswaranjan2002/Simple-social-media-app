dc3678863a2a78b2fcafb0efb2026cae4c6fd4f8
Biswaranjan Sahoo <biswaranjan4864@gmail.com>
2024-12-17 15:18:19 +0530

    port change kiya

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.render("index");
